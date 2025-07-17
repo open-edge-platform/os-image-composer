956de855f2f7c7da6b1cddbbb308a811c93acb20
rpandya28 <96764821+rpandya28@users.noreply.github.com>
2025-07-17 12:07:40 +0530

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -72,7 +72,7 @@ go build ./cmd/image-composer
 
 # Run tests
 echo "Building the Linux image..."
-output=$(echo "user1234" | sudo -S ./image-composer build config/osv/azure-linux/azl3/imageconfigs/defaultconfigs/default-raw-x86_64.yml 2>&1)
+output=$( sudo -S ./image-composer build config/osv/azure-linux/azl3/imageconfigs/defaultconfigs/default-raw-x86_64.yml 2>&1)
 #output=$(sudo bash -c -S './image-composer build config/osv/azure-linux/azl3/imageconfigs/defaultconfigs/default-raw-x86_64.yml 2>&1')
 # Check for the success message in the output
 if echo "$output" | grep -q "image build completed successfully"; then
