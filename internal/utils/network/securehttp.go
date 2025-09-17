29a44659e6271becfc47d09830182406ab7c4bde
Mats Agerstam <mats.g.agerstam@intel.com>
2025-09-17 15:22:20 -0700

    Updated some comments

--- internal/utils/network/securehttp.go
+++ internal/utils/network/securehttp.go
@@ -6,14 +6,13 @@ import (
 )
 
 // NewSecureHTTPClient returns an http.Client with a custom TLS configuration.
-// Callers can reuse this instead of re-defining the TLS settings everywhere.
 func NewSecureHTTPClient() *http.Client {
 
 	tlsConfig := &tls.Config{
 		MinVersion: tls.VersionTLS12,
 		MaxVersion: tls.VersionTLS13,
 
-		// CipherSuites applies only to TLS 1.0–1.2
+		// CipherSuites applies only to TLS 1.2
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
