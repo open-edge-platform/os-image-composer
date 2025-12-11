a2afb3e67701b85fe55dc2336724d293e578c696
Mah, Yock Gen <yock.gen.mah@intel.com>
2025-12-11 14:58:07 +0800

    Addking unit tests for new sbom export routine
    Signed-off-by: Mah, Yock Gen <yock.gen.mah@intel.com>

--- internal/ospackage/debutils/helper.go
+++ internal/ospackage/debutils/helper.go
@@ -9,6 +9,6 @@ import (
 // GenerateSPDXFileName creates a SPDX manifest filename based on repository configuration
 func GenerateSPDXFileName(repoNm string) string {
 	timestamp := time.Now().Format("20060102_150405")
-	SPDXFileNm := filepath.Join("spdx_manifest_rpm_" + strings.ReplaceAll(repoNm, " ", "_") + "_" + timestamp + ".json")
+	SPDXFileNm := filepath.Join("spdx_manifest_deb_" + strings.ReplaceAll(repoNm, " ", "_") + "_" + timestamp + ".json")
 	return SPDXFileNm
 }
