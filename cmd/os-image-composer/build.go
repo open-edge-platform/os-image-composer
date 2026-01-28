7fe1686b648b4a16e63be50b6fcf378ec7d665f9
Mah, Yock Gen <yock.gen.mah@intel.com>
2026-01-28 17:47:23 +0800

    Adding RHEL provider
    Signed-off-by: Mah, Yock Gen <yock.gen.mah@intel.com>

--- cmd/os-image-composer/build.go
+++ cmd/os-image-composer/build.go
@@ -10,6 +10,7 @@ import (
 	"github.com/open-edge-platform/os-image-composer/internal/provider/azl"
 	"github.com/open-edge-platform/os-image-composer/internal/provider/elxr"
 	"github.com/open-edge-platform/os-image-composer/internal/provider/emt"
+	"github.com/open-edge-platform/os-image-composer/internal/provider/rhel"
 	"github.com/open-edge-platform/os-image-composer/internal/provider/ubuntu"
 	"github.com/open-edge-platform/os-image-composer/internal/utils/logger"
 	"github.com/open-edge-platform/os-image-composer/internal/utils/system"
@@ -150,6 +151,10 @@ func InitProvider(os, dist, arch string) (provider.Provider, error) {
 	case ubuntu.OsName:
 		if err := ubuntu.Register(os, dist, arch); err != nil {
 			return nil, fmt.Errorf("registering ubuntu provider failed: %v", err)
+		}
+	case rhel.OsName:
+		if err := rhel.Register(os, dist, arch); err != nil {
+			return nil, fmt.Errorf("registering rhel provider failed: %v", err)
 		}
 	default:
 		return nil, fmt.Errorf("unsupported provider: %s", os)
